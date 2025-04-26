62b45222fe2c4e8339a0b863419b8db8663b7108
beingdeveloper21 <jhaprem4220@gmail.com>
2025-04-26 22:59:24 +0530

    Fix: Update userAuth middleware to send 401 on unauthorized access

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,43 +1,21 @@
-// import jwt from 'jsonwebtoken';
-// const { JsonWebTokenError } = jwt;
-// const userAuth=async(req,res,next)=>{
-//     const {token}=req.cookies;
-//     if(!token){
-//         return res.json({success:false,message:'Not Authorized,Login Again'})
-//     }
-//     try{
-//      const tokenDecode=jwt.verify(token,process.env.JWT_SECRET)
-//      if(tokenDecode.id){
-//         req.body.userId=tokenDecode.id;
-//      }else{
-//         return res.json({success:false,message:'Not Authorized,Login Again'})
-//      }
-//      next();
-//     }
-//     catch(error){
-//         res.json({success:false,message:error.message})
-//     }
-// }
-// export default userAuth;
 import jwt from 'jsonwebtoken';
 const { JsonWebTokenError } = jwt;
-
-const userAuth = async (req, res, next) => {
-    const { token } = req.cookies;
-    if (!token) {
-        return res.status(401).json({ success: false, message: 'Not Authorized, Login Again' });
+const userAuth=async(req,res,next)=>{
+    const {token}=req.cookies;
+    if(!token){
+        return res.json({success:false,message:'Not Authorized,Login Again'})
     }
-    try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-        if (tokenDecode.id) {
-            req.body.userId = tokenDecode.id;
-        } else {
-            return res.status(401).json({ success: false, message: 'Not Authorized, Login Again' });
-        }
-        next();
-    } catch (error) {
-        res.status(401).json({ success: false, message: error.message });
+    try{
+     const tokenDecode=jwt.verify(token,process.env.JWT_SECRET)
+     if(tokenDecode.id){
+        req.body.userId=tokenDecode.id;
+     }else{
+        return res.json({success:false,message:'Not Authorized,Login Again'})
+     }
+     next();
     }
-};
-
+    catch(error){
+        res.json({success:false,message:error.message})
+    }
+}
 export default userAuth;
