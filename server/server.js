0a5d662bde0f95540982e02bcaf7d5856d066d9d
beingdeveloper21 <jhaprem4220@gmail.com>
2025-04-27 13:17:12 +0530

    Updated cookie handling for production and development environments

--- server/server.js
+++ server/server.js
@@ -15,14 +15,7 @@ connectDB();
 const allowedOrigins = ['http://localhost:5173', 'https://mern-auth-pi-peach.vercel.app'];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: allowedOrigins,
     credentials: true,
 }));
 
